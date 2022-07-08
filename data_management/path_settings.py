34c9586af672bf907179e28784aa43aee67fe24a
Yihao Li <alexliyihao@gmail.com>
2022-07-08 16:51:46 -0400

    New data_management folders

---
+++ data_management/path_settings.py
@@ -0,0 +1,28 @@
+ROOT = "/mnt/mfs/hgrcgrid/shared/LPA_analysis"
+
+DATAMETA = {
+    "snps":{
+        "path":"snps",
+        "name_format": "pheno_snps_ethnicity_*.csv"
+    },
+    "genotypes":{
+        "path":"genotypes",
+        "name_format": "geno_ethnicity_*.csv"
+    },
+    "vcfs":{
+        "path":"vcfs",
+        "name_format": "ethnicity_*.vcf"
+    }
+}
+
+COASSIN_OUTPUT = f"{ROOT}/coassin_pipeline/pipeline_output"
+
+ANALYSIS_RESULT_PATH = "data_analysis_result"
+
+CLASS_ALIAS = {"eu": 1,
+               "af": 2,
+               "hisp":3,
+               "others":4,
+               "complete": "complete"}
+
+WHICAP_SOURCE = "/mnt/mfs/hgrcgrid/data/whicap/WHICAP_WES/BAM/washeiDragenBamsList/washeiBamsUpdate2/BQSR/bqsrRGbam"
---
+++ data_management/utils.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+from . import settings
+
+class DotDict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
